30d94a0b1f8967802d543be35e3e3c338f95ae09
oni <rainbow2774@naver.com>
2022-08-22 23:32:44 +0900

    이전에 만든 프레임워크와 스프링 mvc 구조 비교 이해

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,11 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -10,4 +13,9 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+//	@Bean
+//	SpringMemberFormControllerV1 springMemberFormCOntrollerV1() {
+//		return new SpringMemberFormControllerV1();
+//	}
 }
